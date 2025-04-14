b214d725db4c1b8963843155c68177a54eab492e
Aditya Goyal <adityamr.1694@gmail.com>
2025-04-15 00:19:12 +0530

    navbar fixedd for mobile view

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -67,8 +67,8 @@ const Navbar = () => {
 
                 <li><a className='font-Ovo' onClick={closeMenu} href = "#top">Home</a></li>
                 <li><a className='font-Ovo' onClick={closeMenu} href = "#about">About me</a></li>
-                <li><a className='font-Ovo' onClick={closeMenu} href = "#services">Services</a></li>
-                <li><a className='font-Ovo' onClick={closeMenu} href = "#work">My Work</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href = "#projects">Projects</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href = "#coding">Coding Profile</a></li>
                 <li><a className='font-Ovo' onClick={closeMenu} href = "#contact">Contact me</a></li>
             </ul>
 
